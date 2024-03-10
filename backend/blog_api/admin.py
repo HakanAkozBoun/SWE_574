16044eaacd46cb43879c0e0aaf6a9ea61ed069be
ar <90264086+ahmadrezasam@users.noreply.github.com>
2024-03-11 02:16:30 +0300

    backend2 v1

--- backend/blog_api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import category,blog
-# Register your models here.
-
-class blogAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('title',)}
-
-admin.site.register(category)
-admin.site.register(blog,blogAdmin)
