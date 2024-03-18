e0c83576038f2cf43a1f4ddc75c5552d854edffc
ar <90264086+ahmadrezasam@users.noreply.github.com>
2024-03-18 18:11:42 +0300

    Changing relative path of recipe model

--- ui/lib/widgets/home/recipe_list.dart
+++ ui/lib/widgets/home/recipe_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../models/recipe.dart';
+import 'package:recipe/models/recipe.dart';
 
 class RecipeListWidget extends StatefulWidget {
   const RecipeListWidget({Key? key}) : super(key: key);
