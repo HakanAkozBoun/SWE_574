e8bbab8da218323bd5251d663ea98837e762fa1c
Hakan AKOZ <hakan.akoz@std.bogazici.edu.tr>
2024-04-30 14:59:33 +0300

    backendURLChanged

--- ui/lib/constants/backend_url.dart
+++ ui/lib/constants/backend_url.dart
@@ -1,3 +1,4 @@
 class BackendUrl {
-  static const String apiUrl = 'http://127.0.0.1:8000/api/';
+  //static const String apiUrl = 'http://127.0.0.1:8000/api/';
+  static const String apiUrl = 'http://167.172.171.174:8000/api/';
 }
